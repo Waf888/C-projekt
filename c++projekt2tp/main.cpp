a124113ec0af7d50c6235308d967ffb36bf2c86b
Waf888 <113286025+Waf888@users.noreply.github.com>
2024-01-21 21:08:18 +0100

    1main.cpp

--- c++projekt2tp/main.cpp
+++ c++projekt2tp/main.cpp
@@ -1,118 +1,153 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int Menu()
-{
+int Menu() {
     int x;
-    cout<<endl;
-    cout<< "MENU" <<endl<<endl;
-    cout<< "Wybierz numer aby wykonac operacje: "<<endl<<endl;
-    cout<<"1. Dodaj element tablicy x"<<endl;
-    cout<<"2. Wstaw element tablicy"<<endl;
-    cout<<"3. Usun element tablicy"<<endl;
-    cout<<"4. Wyświetl elementy tablicy x"<<endl;
-    cout<<"5. Zapisz do pliku"<<endl;
-    cout<<"6. Wczytaj z pliku"<<endl;
-    cout<<"7. Wyjscie z programu x"<<endl<<endl;
-    cout<<"podaj numer: ";
-    cin>>x;
-
-    cout<<endl;
-
+    cout << endl;
+    cout << "1. Dodaj element tablicy" << endl;
+    cout << "2. Wstaw element tablicy" << endl;
+    cout << "3. Usun element tablicy" << endl;
+    cout << "4. Wyświetl elementy tablicy" << endl;
+    cout << "5. Zapisz do pliku" << endl;
+    cout << "6. Wczytaj z pliku" << endl;
+    cout << "7. Wyjscie z programu" << endl;
+    cin >> x;
+    cout << endl;
     return x;
 }
 
-int main()
-{
+int main() {
     int m;
-    int *p = NULL;
+    int *p = nullptr;
     int n = 0;
-    while(true)
-    {
+
+    while (true) {
         m = Menu();
-        switch(m)
-        {
-        case 7:
-            {
+
+        switch (m) {
+            case 7: {
+                delete[] p;
                 return 0;
             }
-        case 1:
-            {
-                int * tmp = NULL;
-                tmp = new int[n+1]; //tworzenie nowej tablicy (o jeden większej niż poprzednia)
-                if(p != NULL) //jeżeli istnieje poprzednia tablica to:
-                {
-                    for(int i=0;i<n;i++)
-                    {//przekopiuj zawartość poprzedniej tablicy do nowej
+            case 1: {
+                int *tmp = new int[n + 1];
+                if (p != nullptr) {
+                    for (int i = 0; i < n; i++) {
                         tmp[i] = p[i];
                     }
-                    //usuń poprzednią tablicę
                     delete[] p;
-
                 }
-                cout<<"Podaj wartosc elementu tablicy: ";
-                cin>>tmp[n];  //wstaw nową wartość do tablicy (na koniec)
-                p = tmp; //przekopiuj adres nowej tablicy do wskaźnika p
-                n++; //zwiększ ilość elementów tablicy o 1
+                cout << "Podaj wartosc elementu tablicy: ";
+                cin >> tmp[n];
+                p = tmp;
+                n++;
                 break;
             }
-        case 2:
-            {
-                int * tmp = NULL;
-                tmp = new int[n+1]; //tworzenie nowej tablicy (o jeden większej niż poprzednia)
-                if(p != NULL) //jeżeli istnieje poprzednia tablica to:
-                {
-                    for(int i=0;i<n;i++)
-                    {//przekopiuj zawartość poprzedniej tablicy do nowej
+            case 2: {
+                int index;
+                if (n == 0) {
+                    cout << "Tablica jest pusta. Nie mozna wstawic elementu." << endl;
+                    break;
+                }
+                cout << "Podaj indeks, na ktory chcesz wstawic element: ";
+                cin >> index;
+
+                if (index < 0 || index > n) {
+                    cout << "Nieprawidlowy indeks." << endl;
+                } else {
+                    int *tmp = new int[n + 1];
+                    for (int i = 0; i < index; i++) {
                         tmp[i] = p[i];
                     }
-                    //usuń poprzednią tablicę
+                    cout << "Podaj wartosc elementu tablicy: ";
+                    cin >> tmp[index];
+                    for (int i = index + 1; i <= n; i++) {
+                        tmp[i] = p[i - 1];
+                    }
                     delete[] p;
-
+                    p = tmp;
+                    n++;
                 }
-                cout<<"Podaj wartosc elementu tablicy: ";
-                cin>>tmp[n];  //wstaw nową wartość do tablicy (na koniec)
-                p = tmp; //przekopiuj adres nowej tablicy do wskaźnika p
-                n++; //zwiększ ilość elementów tablicy o 1
                 break;
             }
-        case 3:
-            {
-                    for(int i = 0 ; i < n ; i++)
-                        {
+            case 3: {
+                int index;
+                if (n == 0) {
+                    cout << "Tablica jest pusta. Nie ma elementow do usuniecia." << endl;
+                    break;
+                }
+                cout << "Podaj indeks elementu, ktory chcesz usunac: ";
+                cin >> index;
 
-                        delete[] p[i];
+                if (index < 0 || index >= n) {
+                    cout << "Nieprawidlowy indeks." << endl;
+                } else {
+                    int *tmp = new int[n - 1];
+                    for (int i = 0, j = 0; i < n; i++) {
+                        if (i != index) {
+                            tmp[j++] = p[i];
                         }
+                    }
                     delete[] p;
-                break;
-            }
-        case 4:
-            {
-                cout<<"Zawartosc tablicy:"<<endl;
-                for(int i=0;i<n;i++)
-                {
-                    cout<<p[i]<<", ";
+                    p = tmp;
+                    n--;
                 }
-                cout<<endl;
                 break;
             }
-        case 5:
-            {
-                //zapis danych do pliku
+            case 4: {
+                cout << "Zawartosc tablicy:" << endl;
+                for (int i = 0; i < n; i++) {
+                    cout << p[i] << ", ";
+                }
+                cout << endl;
                 break;
             }
-        case 6:
-            {
+            case 5: {
+                cout << "Podaj sciezke dostepu do pliku: ";
+                string sciezka_pliku;
+                cin >> sciezka_pliku;
+                ofstream fout(sciezka_pliku, ios::trunc);
+                if (!fout.is_open()) {
+                    cout << "Nie mozna otworzyc pliku." << endl;
+                    break;
+                }
+                fout << n << endl;
+                for (int i = 0; i < n; i++) {
+                    fout << p[i] << " ";
+                }
+                cout << "Pomyslne zapisano tablice do pliku." << endl;
+                fout.close();
                 break;
             }
-        default:
-            {
-                cout<<"Nieprawidlowa opcja"<<endl;
+            case 6: {
+                cout << "Podaj sciezke dostepu do pliku, do ktorego chcesz zapisac tablice: " << endl;
+                string sciezka_pliku;
+                cin >> sciezka_pliku;
+                ifstream fin(sciezka_pliku);
+                if (!fin.is_open()) {
+                    cout << "Nie mozna otworzyc pliku." << endl;
+                    break;
+                }
+                int rozmiar;
+                fin >> rozmiar;
+                delete[] p;
+                p = new int[rozmiar];
+                for (int i = 0; i < rozmiar; i++) {
+                    fin >> p[i];
+                }
+                n = rozmiar;
+                cout << "Tablica zostala wczytana z pliku." << endl;
+                fin.close();
+                break;
             }
-
+            default: {
+                cout << "Nieprawidlowa opcja" << endl;
+            }
         }
     }
 
     return 0;
 }
+//vsvsvsv
